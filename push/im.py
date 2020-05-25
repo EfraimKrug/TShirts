5e4112659fb0854950c5d9cc8d2c35f30d7892fb
efraim krug <efraimmkrug@gmail.com>
2020-05-25 16:35:37 -0400

    desk top for groups

--- push/im.py
+++ push/im.py
@@ -3,5 +3,5 @@ from ftplib import FTP
 ftp = FTP('ftp.namethatthing.site')
 ftp.login('EfraimFTP','G00dSh@bb0s')
 ftp.cwd('public_html/images')
-ftp.storbinary('STOR exit.png', open('../images/exit.png', 'rb'))
+ftp.storbinary('STOR PDSBBack.gif', open('../images/PDSBBack.gif', 'rb'))
 ftp.quit()
